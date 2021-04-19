b430c0f900d71d5029a60398a13564391c129dd9
daryl <tsdaryl@gmail.com>
2021-04-18 20:18:27 -0700

    create Saved_stimuli model

--- application/models.py
+++ application/models.py
@@ -12,7 +12,7 @@ class Users(db.Model, UserMixin):
   email = db.Column(db.String(100), index=True)
   password = db.Column(db.String(200))
   display_name = db.Column(db.String(100))
-  projects = db.relationship('projects', backref='project_owner', cascade='all,delete')
+  projects = db.relationship('Projects', backref='project_owner', cascade='all,delete')
 
   def __init__(self, email, password, display_name):
     self.email = email
@@ -26,8 +26,8 @@ class Projects(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   ref_id = db.Column(db.Integer, index=True)
   name = db.Column(db.String(100))
-  creation_date = db.Column(db.DateTime)
-  last_accessed = db.Column(db.DateTime)
+  creation_date = db.Column(db.DateTime, default=dt.datetime.now())
+  last_accessed = db.Column(db.DateTime, default=dt.datetime.now())
   user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
   config_JSON = db.Column(db.Text)
 
@@ -38,3 +38,16 @@ class Projects(db.Model):
     self.creation_date = creation_date
     self.last_accessed = last_accessed
     self.config_JSON = config_JSON
+
+class Saved_stimuli(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  project_id = db.Column(db.Integer, db.ForeignKey('projects.id'), index=True)
+  stimulus_JSON = db.Column(db.Text)
+  comments = db.Column(db.Text)
+  saved_date = db.Column(db.DateTime, default=dt.datetime.now())
+
+  def __init__(self, project_id, stimulus_JSON, comments, saved_date):
+    self.project_id = project_id
+    self.stimulus_JSON = stimulus_JSON
+    self.comments = comments
+    self.saved_date = saved_date
