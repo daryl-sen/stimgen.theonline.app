3f6b54237d78d7cd79bcfba590b0c5f32e2cf38f
daryl <tsdaryl@gmail.com>
2021-04-18 20:23:17 -0700

    create project_form

--- application/forms.py
+++ application/forms.py
@@ -6,3 +6,8 @@ class login_form(FlaskForm):
   email = StringField(validators=[DataRequired("Your username is required for login.")])
   password = PasswordField(validators=[DataRequired("Your password is required for login")])
   submit = SubmitField("Log In")
+
+class project_form(FlaskForm):
+  name = StringField('Project Name')
+  create = SubmitField('Create Project')
+  edit = SubmitField('Edit Project')
