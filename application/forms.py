3edea955a4bde997c17c77b8f730f66edadf8280
daryl <tsdaryl@gmail.com>
2021-04-18 20:35:27 -0700

    add coblo_form

--- application/forms.py
+++ application/forms.py
@@ -11,3 +11,18 @@ class project_form(FlaskForm):
   name = StringField('Project Name')
   create = SubmitField('Create Project')
   edit = SubmitField('Edit Project')
+
+class coblo_form(project_form):
+  image_background = StringField('Image Background Color')
+  image_width = StringField('Image Width')
+  image_height = StringField('Image Height')
+  outer_margins = StringField('Outer Margin')
+  central_margins = StringField('Central Margin')
+  object_width = StringField('Object Width')
+  object_height = StringField('Object Height')
+  debug = StringField('Debugging Mode')
+  radius = StringField('Object-Center Radius')
+  fixation_cross_show = StringField('Show Fixation Cross')
+  fixation_cross_color = StringField('Fixation Cross Color')
+  fixation_cross_thickness = StringField('Fixation Cross Thickness')
+  fixation_cross_length = StringField('Fixation Cross Length')
