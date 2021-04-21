e0b5da71d679ed030c372cd43b9192125b226c1e
daryl <tsdaryl@gmail.com>
2021-04-21 13:06:34 -0700

    add dev.py for local dev

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from application import app, PORT
 import waitress
 
 if __name__ == "__main__":
-    # app.run(debug = True, host = '0.0.0.0')
+    app.run(debug = True, host = '0.0.0.0')
     print(f'Serving app on PORT: {PORT}')
-    waitress.serve(app, listen=f"0.0.0.0:{PORT}")
+    # waitress.serve(app, listen=f"0.0.0.0:{PORT}")
---
+++ dev.py
@@ -0,0 +1,5 @@
+from application import app, PORT
+import waitress
+
+if __name__ == "__main__":
+    app.run(debug = True, host = '0.0.0.0')
